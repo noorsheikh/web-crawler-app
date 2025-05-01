566e3fa655b57d0586c66bc9d385b1b34fb933f3
Noor Sheikh <noorsheikh.me@gmail.com>
2025-05-01 19:53:01 -0400

    backend: setup initial django framework and dockerize it

---
+++ backend/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
+
+  echo "PosgreSQL started!"
+fi
+
+# Activate virtualenv
+. /opt/venv/bin/activate
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
