18833cffb181ac16b7f5a8d2878f11c2474be1de
Noor Sheikh <noorsheikh.me@gmail.com>
2025-05-03 10:17:48 -0400

    frontend: setup primereact for ui styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { PrimeReactProvider } from "primereact/api";
+import { createRoot } from "react-dom/client";
+import "primereact/resources/themes/viva-dark/theme.css";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <PrimeReactProvider>
     <App />
-  </StrictMode>,
-)
+  </PrimeReactProvider>
+);
